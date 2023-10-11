f00774d21ed28700c29bdadd4e596525329cf462
Đỗ Đăng Nhật <21dh113939@st.huflit.edu.vn>
2023-10-11 23:00:32 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration, getGenres } from "./store/homeSlice";
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Details from "./pages/details/Details";
